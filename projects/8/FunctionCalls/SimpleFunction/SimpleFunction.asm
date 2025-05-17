999c3f8c2df38b3e0ba9696acff88e4a63baa9e3
Jean Lainee <jean.lainee@gmail.com>
2025-05-17 22:59:07 +0800

    #8 Use proper registry for temp

--- projects/8/FunctionCalls/SimpleFunction/SimpleFunction.asm
+++ projects/8/FunctionCalls/SimpleFunction/SimpleFunction.asm
@@ -143,7 +143,7 @@ M=D
 @5
 A=D-A
 D=M
-@R1
+@R14
 M=D
 
 @SP
@@ -156,30 +156,30 @@ D=A
 @SP
 M=D
 
-@R0
+@R13
 AM=M-1
 D=M
 @THAT
 M=D
 
-@R0
+@R13
 AM=M-1
 D=M
 @THIS
 M=D
 
-@R0
+@R13
 AM=M-1
 D=M
 @ARG
 M=D
 
-@R0
+@R13
 AM=M-1
 D=M
 @LCL
 M=D
 
-@R1
+@R14
 A=M
 0;JMP
